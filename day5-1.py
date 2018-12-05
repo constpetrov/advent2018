530b217fd4a6f9868000e497560f73dfadffe01b
Konstantin Petrov <kpetrov@ripe.net>
2018-12-05 10:53:26 +0100

    Solves the first part of day five

--- day5-1.py
+++ day5-1.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3.7
 
-line = []
+line = list('dabAcCaCBAcCcaDA')
 with open('input5.txt') as f:
     line = list(f.readline().strip())
 
-r_counter = 0
+removed = [0 for i in range(len(line))]
+
+def get_start(removed, start):
+    res = start
+    while removed[res] == 1:
+        res -= 1
+    return res
+
+def get_end(removed, end):
+    res = end
+    while res < len(removed) and removed[res] == 1:
+        res += 1
+    return res
 
 def reductable(line, first, second):
-    if line[first].lower() == line[second].lower():
+    if first < 0:
+        return False
+    elif second > len(line) - 1: 
+        return False
+    elif line[first].lower() == line[second].lower() and line[first] != line[second]:
         return True
     return False
 
-for i in range(len(line) - 1):
-    if reductable(line, i, i + 1):
-        r_counter += 2
+i = 0
+while i < len(line) - 1:
+    start = get_start(removed, i)
+    end = get_end(removed, i + 1)
+    while reductable(line, start, end):
+        print('Line is reductable by letters on positions {} and {}'.format(start, end))
+        removed[start] = 1
+        removed[end] = 1
+        start = get_start(removed, i)
+        end = get_end(removed, i + 1)
+    i = get_end(removed, i + 1)
 
-print(r_counter)
+print(removed.count(0))
